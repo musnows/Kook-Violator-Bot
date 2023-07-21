8bf2a0d37c40bdec82339e12393259ba0e3bf909
musnow <ezplayingd@126.com>
2023-07-21 09:23:32 +0800

    feat: 新增违例用户和服务器配置的查询函数

--- utils/file/SqliteData.py
+++ utils/file/SqliteData.py
@@ -96,12 +96,12 @@ create_tables(
 
 
 async def set_violator_log(guild_id: str,
-                              admin_user_id: str,
-                              user_id: str,
-                              user_name: str,
-                              vol_info: str,
-                              role_id="",
-                              update_time=time.time()):
+                           admin_user_id: str,
+                           user_id: str,
+                           user_name: str,
+                           vol_info: str,
+                           role_id="",
+                           update_time=time.time()):
     """
     参数
     - 传入服务器id，用户id，管理员用户id，违例原因，违例角色组id
@@ -114,7 +114,7 @@ async def set_violator_log(guild_id: str,
     - True（是插入）
     - False（是更新）
     """
-    is_insert = True # 是否是插入
+    is_insert = True  # 是否是插入
     global DBSqlLock
     async with DBSqlLock:
         with sqlite3.connect(DB_NAME) as db:
@@ -127,16 +127,16 @@ async def set_violator_log(guild_id: str,
                               (guild_id, json.dumps([admin_user_id]), user_id,
                                user_name, vol_info, role_id, update_time))
             else:  # 找到了
-                is_insert = False # 是更新
+                is_insert = False  # 是更新
                 # 获取旧值
                 admin_user_list = json.loads(
                     select_ret_all[0][1])  # 原本的管理员用户列表
-                old_vol_info = select_ret_all[0][4] # 原本的违例原因
-                vol_info = old_vol_info + "\n" + vol_info # 拼接新违例原因
+                old_vol_info = select_ret_all[0][4]  # 原本的违例原因
+                vol_info = old_vol_info + "\n" + vol_info  # 拼接新违例原因
                 # 添加管理员用户
                 if admin_user_id not in admin_user_list:
                     admin_user_list.append(admin_user_id)
-                
+
                 # 执行更新sql
                 query.execute(SqliteSql.Update.UPDATE_VIOLATOR_LOG,
                               (json.dumps(admin_user_list), vol_info, role_id,
@@ -150,7 +150,10 @@ async def set_violator_log(guild_id: str,
     return is_insert
 
 
-async def set_guild_conf(guild_id:str,admin_user:list[str],channel_id:str,role_id="e"):
+async def set_guild_conf(guild_id: str,
+                         admin_user: list[str],
+                         channel_id: str,
+                         role_id="e"):
     """配置服务器键值，如果已有，执行更新
     - 如果没有传入role_id，保持旧值不变
     
@@ -158,17 +161,19 @@ async def set_guild_conf(guild_id:str,admin_user:list[str],channel_id:str,role_i
         - True（是插入）
         - False（是更新）
     """
-    is_insert = True # 是否是插入
+    is_insert = True  # 是否是插入
     global DBSqlLock
     async with DBSqlLock:
         with sqlite3.connect(DB_NAME) as db:
             query = db.cursor()
-            select_ret = query.execute(
-                SqliteSql.Select.SELECT_GUILD_CONF, (guild_id, ))
+            select_ret = query.execute(SqliteSql.Select.SELECT_GUILD_CONF,
+                                       (guild_id, ))
             select_ret_all = select_ret.fetchall()
-            if not select_ret_all: # 没有找到
-                query.execute(SqliteSql.Insert.INSERT_GUILD_CONF,(guild_id,json.dumps(admin_user),channel_id,role_id))
-            else: # 找到了
+            if not select_ret_all:  # 没有找到
+                query.execute(
+                    SqliteSql.Insert.INSERT_GUILD_CONF,
+                    (guild_id, json.dumps(admin_user), channel_id, role_id))
+            else:  # 找到了
                 is_insert = False
                 admin_user_list = json.loads(
                     select_ret_all[0][1])  # 原本的管理员用户列表
@@ -179,7 +184,8 @@ async def set_guild_conf(guild_id:str,admin_user:list[str],channel_id:str,role_i
                     if u not in admin_user_list:
                         admin_user_list.append(u)
                 # 更新
-                query.execute(SqliteSql.Update.UPDATE_GUILD_CONF,(admin_user_list,channel_id,role_id,guild_id))
+                query.execute(SqliteSql.Update.UPDATE_GUILD_CONF,
+                              (admin_user_list, channel_id, role_id, guild_id))
 
             db.commit()
 
@@ -187,3 +193,62 @@ async def set_guild_conf(guild_id:str,admin_user:list[str],channel_id:str,role_i
         f"G:{guild_id} | AAu:{admin_user} | C:{channel_id} | rid:{role_id} | guild_conf ({is_insert})"
     )
     return is_insert
+
+
+async def query_violator_log(guild_id: str, user_id=None, user_name=None):
+    """通过用户id或者用户名来查询违例用户，用户名优先
+    - 返回包含用户信息的list[dict]，没找到返回空list
+    """
+    global DBSqlLock
+    select_ret_all, user_info_list = None, []
+    async with DBSqlLock:
+        with sqlite3.connect(DB_NAME) as db:
+            query = db.cursor()
+            if user_id:
+                select_ret = query.execute(
+                    SqliteSql.Select.SELECT_VIOLATOR_LOG_UID,
+                    (guild_id, user_id))
+            elif user_name:
+                select_ret = query.execute(
+                    SqliteSql.Select.SELECT_VIOLATOR_LOG_UNAME,
+                    (guild_id, user_name))
+            # 获取结果
+            select_ret_all = select_ret.fetchall()
+            if not select_ret_all: return []
+
+            # 设置返回值
+            for u in select_ret_all:
+                user_info_list.append({
+                    "guild_id": u[0],
+                    "admin_user": json.loads(u[1]),
+                    "user_id": u[2],
+                    "user_name": u[3],
+                    "vol_info": u[4],
+                    "role_id": u[5],
+                    "update_time": u[6],
+                    "insert_time": u[7]
+                })
+
+    return user_info_list
+
+
+async def query_guild_conf(guild_id: str):
+    """查询服务器配置信息
+    - 返回包含服务器信息的dict
+    - 没找到返回空dict"""
+    global DBSqlLock
+    async with DBSqlLock:
+        with sqlite3.connect(DB_NAME) as db:
+            query = db.cursor()
+            select_ret = query.execute(SqliteSql.Select.SELECT_GUILD_CONF,
+                                       (guild_id, ))
+            select_ret_all = select_ret.fetchall()
+            if not select_ret_all: return {}
+
+            return {
+                "guild_id": select_ret_all[0][0],
+                "admin_user": json.loads(select_ret_all[0][1]),
+                "chanenl_id": select_ret_all[0][2],
+                "role_id": select_ret_all[0][3],
+                "insert_time": select_ret_all[0][4]
+            }
